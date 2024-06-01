631a50938f7f1101fd1042e23519b649077ddea3
Francis <francimarvasconcelosmota3@gmail.com>
2024-06-01 15:28:01 -0300

    primeiro commit

---
+++ process/pizza.php
@@ -0,0 +1,46 @@
+<?php
+
+    include_once("conn.php");
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    if($method === "GET") {
+
+        $bordasQuery = $conn -> query("SELECT * FROM bordas;");
+
+        $bordas = $bordasQuery->fetchAll();
+        
+        $massasQuery = $conn -> query("SELECT * FROM massas;");
+
+        $massas = $massasQuery->fetchAll();
+
+        $saboresQuery = $conn -> query("SELECT * FROM sabores;");
+
+        $sabores = $saboresQuery->fetchAll();
+
+    
+    } else if($method === "POST"){
+
+        $data = $_POST;
+
+        $borda = $data["borda"];
+        $massa = $data["massa"];
+        $sabores = $data["sabores"];
+
+        if(count($sabores)>3){
+
+            $_SESSION["msg"] = "Selecione no máximo 3 sabores!";
+            $_SESSION["status"] = "warning";
+
+        }else{
+
+            echo "passou da validação";
+            exit;
+
+        }
+
+        header("Location: ..");
+
+    }
+
+?>
